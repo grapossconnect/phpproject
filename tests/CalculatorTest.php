426e9f6ba6e028460ab25f4c3264b7ac0c5fddcf
faisal <faisal.grapossconnect@gmail.com>
2024-03-15 16:47:13 +0530

    main

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -8,6 +8,6 @@ class CalculatorTest extends PHPUnit\Framework\TestCase
         $calculator = new App\Calculator;
         $result = $calculator->add(20,5,);
         
-        $this->assertEquals(24, $result);
+        $this->assertEquals(25, $result);
     }
 }

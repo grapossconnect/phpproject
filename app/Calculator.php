f26c5eefa1627c37b9b5db35a3ee21c8b1bc3a72
faisal <faisal.grapossconnect@gmail.com>
2024-03-15 16:52:05 +0530

    main

--- app/Calculator.php
+++ app/Calculator.php
@@ -5,7 +5,7 @@ namespace App;
 class Calculator{
     
     public function add($num1, $num2) {
-        return $num1 + $num2;
+        return $num1 - $num2;
     }
 
     public function subtract($num1, $num2) {
